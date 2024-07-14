c96763223764b550d1c7b70e237286980c3bfc60
Majdi Zerelli <zerellimajdi@gmail.com>
2024-07-14 12:38:10 +0100

    add alias sh script

--- add_alias.sh
+++ add_alias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git config --global alias.tag-and-push '!f() { git tag -f $1 && git push -f origin $1 && git push -f origin main; }; f'
+git config --global alias.tag-and-push '!f() { git tag -f $1 && git push -f origin $1 && git merge latest && git push origin main; }; f'
